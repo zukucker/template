6886fd865bdd426bae0c9beca561621678443661
zukucker <alexander.froehling@googlemail.com>
2020-04-20 14:44:38 +0200

    template

---
+++ vue.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el: '#settings',
+    data: {
+        settings: 'Einstellungen'
+    },
+})
+
+var app = new Vue({
+    el: '#weather',
+    data: {
+        weather: 'Wetter'
+    }
+})
+
+var app = new Vue({
+    el: '#dailyqoute',
+    data: {
+        qoute: 'Zitat'
+    }
+})
+
+var app = new Vue({
+    el: '#time',
+    data: {
+        time: 'Uhrzeit'
+    }
+})
+
+var app = new Vue({
+    el: '#todo',
+    data:{
+        todo: 'ToDo'
+    }
+})
